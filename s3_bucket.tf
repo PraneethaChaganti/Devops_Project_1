d41299dc301c603186f2e8e890105f595673e7e9
Praneetha Chaganti <praneethachaganti@gmail.com>
2026-05-29 20:40:49 -0400

    Simplify vault_token handling in Terraform pipeline
    Centralized the `vault_token` configuration in Terraform by:
    - Removing explicit passing of `vault_token` in Jenkins pipeline.
    - Adding `token` attribute to Vault provider in `s3_bucket.tf`.
    - Introducing a sensitive `vault_token` variable in `variables.tfvars`.
    
    These changes improve security and maintainability by leveraging Terraform's variable handling and ensuring seamless integration with the Vault provider.

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -16,6 +16,7 @@ terraform {
 
 provider "vault" {
 	address = "http://127.0.0.1:8200"
+	token="var.vault_token"
 }
 
 data "vault_aws_access_credentials" "creds" {
---
+++ variables.tfvars
@@ -0,0 +1,5 @@
+variable "vault_token" {
+  description = "The token"
+  type        = string
+  sensitive   = true
+  }
