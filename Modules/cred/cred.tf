ce7bb86f626e2b486f08c1f3dae78fdc80762920
Praneetha Chaganti <praneethachaganti@gmail.com>
2026-06-04 15:14:24 -0400

    Refactor: Replace versioning module with local provider
    Removed the `versioning` module previously sourced from
    `./Modules/versioning`. Added a `local` provider configuration
    to the `terraform` block, specifying `hashicorp/local` as the
    source and setting the version constraint to `~> 2.0`. This
    change reflects a shift in infrastructure configuration to
    use the `local` provider instead of the `versioning` module.

--- Modules/cred/cred.tf
+++ Modules/cred/cred.tf
@@ -1,9 +1,9 @@
-module "versioning" {
-	source = "./Modules/versioning"
-}
-
 terraform {
 	required_providers {
+		local = {
+			source = "hashicorp/local"
+			version = "~> 2.0"
+		}
 		vault = {
 			source = "hashicorp/vault"
 			version = "2.0.0"
