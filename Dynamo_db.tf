6dd510281cddeab377257079afcd0b7e34fcf87a
Praneetha Chaganti <praneethachaganti@gmail.com>
2026-06-11 22:41:32 -0400

    Update S3 backend and DynamoDB table tags
    The `terraform` block's S3 backend configuration was updated:
    - Changed the `bucket` value to `s3-bucket-2026-terraform-state-file`.
    - Retained other backend settings (key, region, dynamodb_table).
    
    The `resource aws_dynamodb_table` block was updated to include new tags:
    - Added `Environment = "Production"`.
    - Added `ManagedBy = "Terraform"`.

--- Dynamo_db.tf
+++ Dynamo_db.tf
@@ -2,15 +2,6 @@ module "versioning" {
 	source = "./Modules/cred"
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "your-s3-state-bucket"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "dynamodb-table-2026-terraform-state-locking"
-  }
-}
-
 resource aws_dynamodb_table "dynamodb_table" {
   name         = "dynamodb-table-2026-terraform-state-locking"
   billing_mode = "PAY_PER_REQUEST"
@@ -23,4 +14,14 @@ resource aws_dynamodb_table "dynamodb_table" {
   Environment = "Production"
   ManagedBy   = "Terraform"
 	}
+}
+
+
+terraform {
+  backend "s3" {
+    bucket         = "s3-bucket-2026-terraform-state-file"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "dynamodb-table-2026-terraform-state-locking"
+  }
 }
