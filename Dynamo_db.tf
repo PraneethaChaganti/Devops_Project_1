5d3a919313e88ce34cff570aaef6e3fd86002400
Praneetha Chaganti <praneethachaganti@gmail.com>
2026-06-15 11:41:50 -0400

    Refactor AWS provider configuration
    Moved AWS provider configuration from `cred.tf` to `Dynamo_db.tf`.
    Added `terraform` block in `Dynamo_db.tf` to define the `aws`
    provider with version `4.67.0` and region `us-east-1`. Introduced
    a new DynamoDB table resource for state locking in `Dynamo_db.tf`.
    Removed redundant AWS provider configuration from `cred.tf`.

--- Dynamo_db.tf
+++ Dynamo_db.tf
@@ -2,6 +2,19 @@ module "versioning" {
 	source = "./Modules/cred"
 }
 
+terraform {
+	required_providers {
+		aws = {
+			source = "hashicorp/aws"
+			version = "4.67.0"
+		}
+	}
+}
+
+provider "aws" {
+	region = "us-east-1"
+} 
+
 resource aws_dynamodb_table "dynamodb_table" {
   name         = "dynamodb-table-2026-terraform-state-locking"
   billing_mode = "PAY_PER_REQUEST"
--- Modules/cred/cred.tf
+++ Modules/cred/cred.tf
@@ -8,17 +8,9 @@ terraform {
 			source = "hashicorp/vault"
 			version = "2.0.0"
 		}
-		aws = {
-			source = "hashicorp/aws"
-			version = "4.67.0"
-		}
 	}
 }
 
 provider "vault" {
 	address = "http://172.18.0.3:8200" 
 }
-
-provider "aws" {
-	region = "us-east-1"
-} 
